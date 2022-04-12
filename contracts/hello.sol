db9ef060f7388c195d012d12569efc0f2919f3f0
Andrew Elkins <Andrew.j.elkins@gmail.com>
2022-04-12 00:58:42 -0400

    first commit

---
+++ contracts/hello.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.0;
+
+contract Helloworld{
+    string message = "Hello world";
+
+    function setMessage(string memory newMessage) public payable {
+        message = newMessage;
+    }
+    function hello() public view returns (string memory) {
+        return message;
+    }
+}
